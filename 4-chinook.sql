8c16a83e9068a6b01436ea7bdb97f1a5889185f4
Joaquin Pirillo <joaquinpirillo5102007@gmail.com>
2025-08-21 11:52:52 -0300

    TERMINE CHINOOK

--- 4-chinook.sql
+++ 4-chinook.sql
@@ -16,5 +16,47 @@ GROUP by a.Title
 HAVING cantidad >= 5 
 ORDER by cantidad ASC
 /*5)-----------------------------------------------------*/
-
+SELECT a.title,  sum(t.UnitPrice) as Precio_Album FROM albums a
+INNER JOIN tracks t on a.albumid = t.albumid
+GROUP by  a.title 
+ORDER by Precio_Album DESC
+LIMIT 10
 /*6)-----------------------------------------------------*/
+SELECT t.name, g.name, a.title FROM tracks t
+INNER JOIN genres g on t.genreid = g.genreId
+INNER JOIN albums a on t.albumId = a.albumId 
+WHERE t.UnitPrice like 0.99 
+/*7)-----------------------------------------------------*/
+SELECT t.name, t.Milliseconds, a.title, artists.name FROM tracks t
+INNER JOIN albums a on t.albumId = a.albumId 
+INNER JOIN artists on a.ArtistId = artists.ArtistId
+ORDER by t.Milliseconds ASC
+LIMIT 20
+/*8)-----------------------------------------------------*/
+SELECT e.LastName as Empleado, e.Title, j.LastName as Jefe, 
+count(c.CustomerId) as Cantidad_Clientes FROM employees j
+INNER JOIN employees e on j.EmployeeId = e.ReportsTo 
+INNER JOIN customers c on e.EmployeeId = c.SupportRepId  
+GROUP by Empleado ORDER by Cantidad_Clientes DESC
+/*9)-----------------------------------------------------*/
+INSERT INTO tracks(name, MediaTypeId, Milliseconds, UnitPrice)
+VALUES ("cancion_1", 1, 1000, 0.99), ("cancion_2", 1, 1000, 0.99), 
+("cancion_3", 1, 1000, 0.99), ("cancion_4", 1, 1000, 0.99)
+/*10)-----------------------------------------------------*/
+SELECT * FROM tracks t
+ORDER by t.TrackId DESC
+LIMIT 8
+/*11)-----------------------------------------------------*/
+UPDATE tracks
+SET name = "cancion_4_modificada"
+WHERE TrackId = 3507
+
+UPDATE tracks
+SET name = "cancion_3_modificada"
+WHERE TrackId = 3506
+/*12)-----------------------------------------------------*/
+SELECT * FROM tracks
+WHERE name like "%modificada"
+/*13)-----------------------------------------------------*/
+DELETE FROM tracks
+WHERE TrackId BETWEEN 3508 AND 3511
