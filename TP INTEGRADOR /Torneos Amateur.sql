769b35706ae251f148ef4d644fc651b3546bbf2f
Joaquin Pirillo <joaquinpirillo5102007@gmail.com>
2025-09-18 11:20:13 -0300

    Base de datos TorneosAmateur

--- TP INTEGRADOR /Torneos Amateur.sql
+++ TP INTEGRADOR /Torneos Amateur.sql
@@ -1,93 +1,74 @@
---Creo las 4 tablas con sus respetivos campos a completar
 
 CREATE TABLE "Equipos" (
-	"Nombre"	VARACHAR(25) NOT NULL,
-	"Deporte"	VARACHART(25) NOT NULL,
+	"Nombre"	VARCHAR(25) NOT NULL,
+	"Deporte"	VARCHAR(25) NOT NULL,
 	"Jugadores" INTEGER,
 	"EquipoId" INTEGER,
-	PRIMARY KEY("EquipoId" AUTOINCREMENT)
+	PRIMARY KEY("EquipoId" AUTO_INCREMENT)
 );
 
 CREATE TABLE "Jugadores" (
-	"Nombre"	VARACHAR(25) NOT NULL,
-	"Apellido"	VARACHAR(25) NOT NULL,
+	"Nombre"	VARCHAR(25) NOT NULL,
+	"Apellido"	VARCHAR(25) NOT NULL,
 	"EquipoId" INTEGER NOT NULL,
 	"JugadorId"	INTEGER,
-	PRIMARY KEY("jugadorId" AUTOINCREMENT),
+	PRIMARY KEY("JugadorId" AUTO_INCREMENT),
 	FOREIGN KEY("EquipoId") REFERENCES "Equipos"("EquipoId")
 );
 
---------------------------------------------------------------------------------------
+CREATE TABLE "Inscripciones" (
+	"Deuda" INT NOT NULL,
+    "EquipoId" INT NOT NULL,
+    "TorneoId" INT NOT NULL,
+    "InscripcionId" INT,
+    PRIMARY KEY("InscripcionId" AUTO_INCREMENT),
+    FOREIGN KEY("EquipoId") REFERENCES "Equipos"("EquipoId"),
+    FOREIGN KEY("TorneoId") REFERENCES "Torneos"("TorneoId")
+);
 
 CREATE TABLE "Torneos" (
-	"Deporte" VARACHAR(25) NOT NULL,
-	"Terminado?" INTEGER NOT NULL,
-	"EquipoId" INTEGER,
-	"TablasTorneosId" INTEGER,
-	"TorneoId"	INTEGER,
-	PRIMARY KEY("TorneoId" AUTOINCREMENT),
-	FOREIGN KEY("EquipoId") REFERENCES "Equipos"("EquipoId"),
-	FOREIGN KEY("TablasTorneosId") REFERENCES "TablasTorneos"("TablasTorneosId")
+	"Deporte" VARCHAR(25) NOT NULL,
+    "FechaInicial" DATE NOT NULL,
+    "FechaFinal" DATE NOT NULL,
+    "Ganador" VARCHAR(25),
+    "TorneoId" INT,
+    PRIMARY KEY("TorneoId" AUTO_INCREMENT)
 );
 
-CREATE TABLE "Partidos" (
-	"Fecha" VARACHAR(25) NOT NULL,
-	"Hora" VARACHAR(25) NOT NULL,
-	"Lugar" VARACHAR (50) NOT NULL,
-	"EquipoA" VARACHAR(25) NOT NULL,
-	"EquipoB" VARACHAR(25) NOT NULL,
-	"Resultado" VARACHAR(25) NOT NULL,
-	"Suspendido?" VARACHAR(25) NOT NULL,
-	"PartidoId" INTEGER,
-	PRIMARY KEY("PartidoId" AUTOINCREMENT),
+CREATE TABLE "Estadisticas" (
+	"PuntajeTorneo" INT NOT NULL,
+    "Estadistica" INT NOT NULL,
+    "EquipoId" INT NOT NULL,
+    "TorneoId" INT NOT NULL,
+    "EstadisticaId" INT,
+    PRIMARY KEY("EstadisticaId" AUTO_INCREMENT)
+    FOREIGN KEY("EquipoId") REFERENCES "Equipos"("EquipoId"),
+    FOREIGN KEY("TorneoId") REFERENCES "Torneos"("TorneoId")
+ 
 );
 
---CURRENT_TIMESTAMP
+CREATE TABLE "Fixture" (
+	"Fecha" DATE NOT NULL,
+    "Hora" TIME NOT NULL,
+    "Lugar" VARCHAR(25) NOT NULL,
+    "EquipoA" VARCHAR(25) NOT NULL,
+    "EquipoB" VARCHAR(25) NOT NULL,
+    "Resultado" VARCHAR(25) NOT NULL,
+    "TorneoId" INT NOT NULL,
+    "FixtureId" INT,
+    PRIMARY KEY("FixtureId" AUTO_INCREMENT),
+    FOREIGN KEY("TorneoId") REFERENCES "Torneos"("TorneoId")
+);
 
+CREATE TABLE "Arbitraje"(
+	"PuntajePartido" INT NOT NULL 
+	"FixtureId" INT NOT NULL
+	"EstadisticaId" INT NOT NULL 
+	"EquipoId" INT NOT NULL
+	"ArbritajeId" INT
+    PRIMARY KEY("ArbritajeId" AUTO_INCREMENT)
+    FOREIGN KEY("FixtureId") REFERENCES "Fixture"("FixtureId")
+    FOREIGN KEY("EstadisticaId") REFERENCES "Estadisticas"("EstadisticaId")
+    FOREIGN KEY("EquipoId") REFERENCES "Equipos"("EquipoId")
+);
 --------------------------------------------------------------------------------------
-
-CREATE TABLE "TablasTorneos" (
-	"PuntajeTorneo" INTEGER NOT NULL,
-	"PuntajePartidoId" INTEGER NOT NULL,
-	"EquipoId" INTEGER NOT NULL,
-	"TorneoId" INTEGER NOT NULL,
-    "TablasTorneoId" INTEGER,
-	PRIMARY KEY("TablasTorneoId" AUTOINCREMENT),
-	FOREIGN KEY("EquipoId") REFERENCES "Equipos"("EquipoId"),
-	FOREIGN KEY("TorneoId") REFERENCES "Torneos"("TorneoId"),
-	FOREIGN KEY("PuntajePartidoId") REFERENCES "TablasPartidos"("PuntajePartidoId")
-);
-
-CREATE TABLE "TablasPartidos" (
-	"PuntajePartido" INTEGER NOT NULL,
-	"EquipoId" INTEGER NOT NULL,
-	"PartidosId" INTEGER NOT NULL,
-    "TablasPartidoId" INTEGER,
-	PRIMARY KEY("TablasPartidoId" AUTOINCREMENT),
-	FOREIGN KEY("EquipoId") REFERENCES "Equipos"("EquipoId"),
-	FOREIGN KEY("PartidoId") REFERENCES "Partidos"("PartidoId")
-);
-
---------------------------------------------------------------------------------------
-
---6 Equipos
---12 Jugadores
---2 Torneos
---Puntaje
-
-INSERT INTO Equipos(Nombre, Deporte, Jugadores)
-VALUES ("Los Campeones", "Futbol", 12), ("Rapidos Y Felices", "Futbol", 7),
-("Juanes", "El que pinte", 20), ("Los polenta", "Ajedres", 3),
-("Los Profes", "Ajedres", 10), ("Subime los puntos gil", "Futbol",1);
-
-INSERT INTO Jugadores(Nombre, Apellido, EquipoId)
-VALUES 
-("Diego", "Villace", 1), ("Tomas", "Aragon", 1), 
-("Santi", "Pardo", 2), ("Lautaro", "Oratual", 2),
-("Juan", "Perez", 3), ("Juan", "Mendez", 3),
-("Walter", "Carraje", 4), ("Roberto", "Socrates", 4), 
-("Andres", "Navarro", 5), ("Eduardo", "Mestrovich", 5), ("Julian", "Impeluso", 5),
-("Joaquin", "Pirillo", 6);
-
-INSERT INTO Torneos(Deporte, EquipoId)
-VALUES ("Boxeo Acuatico", 3), ("Ajedres", 5);
